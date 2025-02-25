6cc82a743ee697e6c8bbcb7bc3cb7b3a01c78bfa
RomanShevchuk0 <roman.shevchuk@dmorpheus.design>
2025-02-25 19:25:57 +0100

    improved profile

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -13,15 +13,19 @@ const EditProfile = () => {
     <div className="max-w-md mx-auto mt-10 p-6 shadow-lg rounded-2xl">
       <div className="flex justify-between items-center mb-4">
         <h1 className="text-3xl font-bold">Profile</h1>
-        <Button variant="outline" onClick={() => setIsProfileIditing(true)}>
-          Edit
+        <Button variant="outline" onClick={() => setIsProfileIditing((prev) => !prev)}>
+          {isProfileIditing ? "Cancel" : "Edit"}
         </Button>
       </div>
       <div className="flex flex-col gap-10">
         {isProfileIditing ? (
-					<AvatarForm />
-					) : (
-          <img className="rounded-md" src={user?.avatarUrl || defaultAvatar} alt="Avatar" />
+          <AvatarForm />
+        ) : (
+          <img
+            className="aspect-square object-cover rounded-md"
+            src={user?.avatarUrl || defaultAvatar}
+            alt="Avatar"
+          />
         )}
 
         <ProfileForm />
